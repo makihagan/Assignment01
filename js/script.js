e152bcf81b866bba8adfbfec413a2c614c239c09
maki <mamajohnjohn@gmail.com>
2022-02-22 13:12:12 -0800

    finished assignment

---
+++ js/script.js
@@ -0,0 +1,121 @@
+// STEP 1
+// let someMonth;
+// let theMonth;
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
+// STEP 2
+// let numericLiteral = 40;
+// let stringLiteral = 'Hello, I am string.';
+// let booleanLiteral = true;
+// let nullLiteral = null;
+
+// STEP 3
+// let varExpression = 'Variables';
+// let complexExpression = 10 + 20;
+
+// STEP 4
+// let firstName;
+// let lastName;
+// let iAddress;
+// let iCity;
+// let iState;
+// let zipCode;
+// let yourAge;
+// let referralSource;
+// let mayWeContactYou;
+
+// STEP 5
+// var firstName = 'Maki';
+// let lastName = 'Hagan';
+// const mayWeContactYou = 'May we contact you?';
+
+// STEP 6
+// let myZip = 'My zip code is ' + 92109;
+// console.log (myZip);
+
+// STEP 7
+// let firstVariable = true + ' that means correct.';
+// console.log (firstVariable);
+// let secondVariable = 5 + false;
+// console.log (secondVariable);
+
+// STEP 8
+// the apostrophe needs a back slash before it
+// let someString = 'Who once said, "only two things are infinite, the universe and human stupidity and I\'m not sure about the former."';
+// window.console.log(someString);
+
+// STEP 9 
+// let nullValue = null;
+// console.log(nullValue);
+// let undefinedValue;
+// console.log(undefinedValue);
+
+// STEP 10
+// let x = 5;
+// let y = 10;
+// let z;
+// let n = null;
+// let theAnswerIs = 'The answer is... ';
+// let stringType = theAnswerIs + x + y;
+// let numberType = x + y;
+// let booleanType = x == y;
+// let objectType = n;
+// let undefinedType = x = z;
+// console.log(typeof stringType);
+// console.log(typeof numberType);
+// console.log(typeof booleanType);
+// console.log(typeof objectType);
+// console.log(typeof undefinedType);
+
+// STEP 11
+// alert('Hello ' + 'Maki Hagan' + ', welcome to the JavaScript class!');
+
+// STEP 12
+// let name = 'Maki Hagan';
+// alert('Hello ' + name + ', welcome to the JavaScript class!');
+
+// STEP 13
+// let course = 'JavaScript';
+// alert('Hello ' + name + ', welcome to the ' + course + ' class!');
+
+// STEP 14
+// alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!');
+
+// STEP 15
+// let yourName = prompt('Enter your name');
+// alert('Hello ' + yourName + '.\nWelcome to the ' + course + ' class!');
+
+// STEP 16
+// let className = prompt('Enter your class name');
+// alert('Hello ' + yourName + '.\nWelcome to the ' + className + ' class!');
+
+// STEP 17
+// let x = 10;
+// let y = 20;
+// console.log(x + y);
+
+// STEP 18
+// let x = 20;
+// x += 20;
+// console.log(x);
+
+// STEP 19
+// let x = 20
+// x *= 5;
+// console.log(x);
+
+// STEP 20
+// let x = 20 % 3;
+// console.log(x);
+
+// STEP 21
+let x = 5;
+let y = 10;
+let z = 2;
+console.log(y == x * z && x + z <= y);
+
+// STEP 22
+// using same variables as STEP 21
+console.log(x / z == y || x + z >= y);
